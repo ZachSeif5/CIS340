13fe3b4a81285becac0e570b10e2bb54ee4f5c53
ZachSeif5 <zachseif@colostate.edu>
2024-04-11 16:02:37 -0600

    MySecondProject Week 13

---
+++ MySecondProject/App copy 2.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import {Text} from 'react-native';
+
+export default function MyApp() {
+
+  function getFullName(fName, mName, lName) {
+    return fName + " " + mName + " " + lName;
+  }
+
+  const pet = "Dog";
+  return (
+  
+      <Text>
+        {"\n\n\n\n\n\n"}
+        Hello!, I am a student in CIS340! {"\n"}
+        My full name is {getFullName("Zachery", "Cain", "Seifert")} {"\n"}
+        I have a {pet}!
+        </Text>
+  
+
+  
+  );
+}
+
+
---
+++ MySecondProject/App copy 3.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import {Text} from 'react-native';
+
+export default function MyApp() {
+  return (
+    <View>
+      <Text>
+        {"\n\n\n\n\n\n"}
+        Hello, I am a student in CIS340! {"\n"}
+      </Text>
+        style = {{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1
+        }}
+
+    </View>
+  );
+}
+
+
