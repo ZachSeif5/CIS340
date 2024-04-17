5ba5d233009f38e9239660e182a14f78a8f1d9c8
ZachSeif5 <zachseif@colostate.edu>
2024-04-17 17:46:50 -0600

    MyFourthProject

---
+++ MyFourthProject/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Text, Image, ScrollView } from 'react-native';
+
+const dog = {
+  uri: 'https://github.com/AbdunabiRamadan/CIS340-Images/blob/main/images/dog2.png',
+  width: 64,
+  height: 64
+};
+
+export default MyScrollViewApp = () => (
+  
+   
+      <ScrollView style={{padding: 40}}>
+        <Text style={{fontSize: 80}}> Try to scroll down </Text>
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Text style={{fontSize: 80}}> Try to scroll down again, if you like </Text>
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Text style={{fontSize: 80}}> Try to scroll down </Text>
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Text style={{fontSize: 80}}> Try to scroll down </Text>
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Text style={{fontSize: 80}}> React Native Components!</Text>
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+        <Image source={dog} />
+       </ScrollView>
+  );
